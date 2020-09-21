3e296eb6b367e01c3282be89c126b80645bd80e9
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-09-21 19:58:59 +0100

    Setting up eslint and tsconfig

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: ['eslint:recommended'],
+	parserOptions: {
+		parser: '@typescript-eslint/parser',
+	},
+	rules: {
+		'no-var': 'error',
+		'@typescript-eslint/no-unused-vars': 'error',
+		'indent': ['error', 'tab'],
+		'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
+		'semi': ['error', 'never'],
+		'quotes': ['error', 'single'],
+		'prefer-const': ['error'],
+		'arrow-parens': ['error', 'always'],
+	},
+	overrides: [
+		{
+			files: ['test/**/*.spec.{j,t}s'],
+			env: {
+				jest: true,
+			},
+		},
+	],
+}
